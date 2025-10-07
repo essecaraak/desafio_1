6252126a3601311c5d4de1147a22c2dcb7f72bec
essecaraak <matheusoliveiradesouza190503@gmail.com>
2025-10-07 15:57:45 -0300

    projeto feito no final do treinamento

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container,Content } from "./styles";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import { Column } from "./styles";
+import { Row} from "./styles";
+import { useState } from "react";
 
-function App() {
+const App = () =>{
+  const [currentNumber,setCurrentNumber] = useState('0');
+  const [firstNumber,setFirstNumber] = useState('0');
+  const [operation,setOperation] = useState('');
+
+  const handleAddNumber = (number) =>{
+    setCurrentNumber(prev=>`${prev === '0' ? '': prev}${number}`)
+  }
+  const handleOnClear = () =>{
+    setCurrentNumber('0');
+    setOperation('');
+    setFirstNumber('0');
+    
+  }
+
+  const handleSumNumbers = () =>{
+    if(firstNumber ==='0'){
+      setFirstNumber(currentNumber);
+      setCurrentNumber('0');
+      setOperation('+');
+    }else{
+      const sum = Number(firstNumber)+ Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('');
+      setFirstNumber('0');
+    }
+  }
+  const handleMinusNumbers = () =>{
+    if(firstNumber ==='0'){
+      setFirstNumber(currentNumber);
+      setCurrentNumber('0');
+      setOperation('-');
+    }else{
+      const sum = Number(firstNumber)- Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('');
+      setFirstNumber('0');
+    }
+  }
+  const handleEquals= () =>{
+    if(firstNumber !=='0' && operation!=='' && currentNumber !=='0'){
+      switch(operation){
+        case '+':
+          handleSumNumbers();
+          break;
+        case '-':
+          handleMinusNumbers();
+          break;
+        default:
+          break;
+      }
+      
+      
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="x" onClick={()=>handleAddNumber('7')}/>
+          <Button label="/" onClick={()=>handleAddNumber('7')}/>
+          <Button label="c" onClick={()=>handleOnClear()}/>
+          <Button label="X" onClick={()=>handleAddNumber('7')}/>
+        </Row>
+        <Row>
+          <Button label="7" onClick={()=>handleAddNumber('7')}/>
+          <Button label="8" onClick={()=>handleAddNumber('8')}/>
+          <Button label="9" onClick={()=>handleAddNumber('9')}/>
+          <Button label="-" onClick={()=>handleMinusNumbers()}/>
+        </Row>
+        <Row>
+          <Button label="4" onClick={()=>handleAddNumber('4')}/>
+          <Button label="5" onClick={()=>handleAddNumber('5')}/>
+          <Button label="6" onClick={()=>handleAddNumber('6')}/>
+          <Button label="+" onClick={()=>handleSumNumbers()}/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={()=>handleAddNumber('1')} />
+          <Button label="2" onClick={()=>handleAddNumber('2')}/>
+          <Button label="3" onClick={()=>handleAddNumber('3')}/>
+          <Button label="=" onClick={()=>handleEquals()}/>
+        </Row>
+        
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color:#AAAAFF;
+
+    display:flex;
+    align-content:center;
+    justify-content: flex-end;
+
+    font-size:24px;
+    font-family: 'Roboto';
+    
+
+    Input{
+        width:100%;
+        height: 75px;
+        background-color:#AAAAFF;
+        border:0;
+        float: right;
+        clear: both;
+        text-align: right;
+        font-size:24px;
+        font-family: 'Roboto';
+        padding-right: 10px;
+        color: #FFFFFF
+    }
+`
